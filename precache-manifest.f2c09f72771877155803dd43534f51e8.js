12d0ae791cfcdf42f09abe8c637a7b6ba085803e
Jeffreyo3 <Jeffrey.Orndorff@gmail.com>
2019-09-30 17:57:42 -0700

    Updates

---
+++ precache-manifest.f2c09f72771877155803dd43534f51e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9e19c4eb5a1e39933586fbe6a76681",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0749df78825d1b82b008",
+    "url": "/static/css/main.cd855098.chunk.css"
+  },
+  {
+    "revision": "dcdf05b55f333318bf51",
+    "url": "/static/js/2.62c45116.chunk.js"
+  },
+  {
+    "revision": "0749df78825d1b82b008",
+    "url": "/static/js/main.15685ba2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1abd8c1a58598974d8415573764ba76a",
+    "url": "/static/media/Ozone.1abd8c1a.ttf"
+  }
+]);
